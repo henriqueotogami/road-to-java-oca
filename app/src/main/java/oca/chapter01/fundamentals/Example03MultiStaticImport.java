531e2d29d454d8b689bef696983b26447757e10d
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-02-04 23:55:21 -0300

    Issue #7 - Pacotes, Imports e Imports Estáticos em Java

---
+++ app/src/main/java/oca/chapter01/fundamentals/Example03MultiStaticImport.java
@@ -0,0 +1,18 @@
+package oca.chapter01.fundamentals;
+
+/* Importa todos os membros estáticos da classe Locale */
+import static java.util.Locale.*;
+/**
+ * Exemplo de importação estática múltipla em Java.
+ * Mostra como importar todos os membros estáticos de uma classe.
+ * @author henriqueotogami
+ * @since 2026-02-04
+ * @version 1.0
+ */
+public class Example03MultiStaticImport {
+    public static void main(String[] args) {
+        System.out.println("Locale: " + ITALY); // Exibe "Local: it_IT"
+        System.out.println("Locale: " + GERMANY); // Exibe "Local: de_DE"
+        System.out.println("Locale: " + JAPANESE); // Exibe "Local: ja"
+    }
+}
