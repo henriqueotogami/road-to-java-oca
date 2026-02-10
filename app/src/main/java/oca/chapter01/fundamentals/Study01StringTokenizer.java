88c7fc40fb4fde8367a406957d01fd701e92afce
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-02-09 23:36:37 -0300

    Issue 7 - Classes Derivadas de Pacotes: APIs de Utils e io. Estudo de String Tokenizer.

---
+++ app/src/main/java/oca/chapter01/fundamentals/Study01StringTokenizer.java
@@ -0,0 +1,23 @@
+package oca.chapter01.fundamentals;
+
+import java.util.StringTokenizer;
+
+/**
+ * Exemplo de uso da classe StringTokenizer em Java.
+ * A classe StringTokenizer é usada para dividir uma string em tokens com base em um delimitador especificado.
+ * @author henriqueotogami
+ * @since 2026-02-09
+ * @version 1.0
+ */
+public class Study01StringTokenizer {
+
+    public static void main(String[] args) {
+
+        final StringTokenizer stringTokenizer = new StringTokenizer("um,dois,três", ",");
+
+        while (stringTokenizer.hasMoreTokens()) {
+            System.out.println(stringTokenizer.nextToken());
+        }
+
+    }
+}
