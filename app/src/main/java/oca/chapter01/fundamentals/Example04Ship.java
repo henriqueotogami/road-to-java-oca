f4aa7f0fa8e423f46291035335879f830a21fed6
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-02-12 22:35:07 -0300

    Issue #7 Exemplo 04 de Estruturas de Classes

---
+++ app/src/main/java/oca/chapter01/fundamentals/Example04Ship.java
@@ -0,0 +1,22 @@
+package oca.chapter01.fundamentals;
+
+/**
+ * <br>Classe abstrata que define as características básicas de uma nave.
+ * <br>Esta classe é estendida pela classe Example04Spaceship, que representa uma nave espacial específica.
+ * @author henriqueotogami
+ * @since 2026-02-12
+ * @version 1.0
+ * @see Example04Spaceship          Classe que representa uma nave espacial
+ * @see Example04SpaceshipSimulator Classe que simula a criação de uma nave
+ * @see Example04Dockable           Interface que define o comportamento de acoplamento para naves
+ */
+public abstract class Example04Ship {
+
+    public enum ShipType {
+        FRIGATE, BATTLESHIP, MINELAYER, ESCORT, DEFENSE
+    }
+
+    public Example04Ship() {
+        System.out.println("\nExample04Ship() - Ship created with default ship type.");
+    }
+}
