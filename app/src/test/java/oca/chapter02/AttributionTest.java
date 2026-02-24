64865e74e82bc709109be3411cc4a57b2dab1747
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-02-23 23:41:43 -0300

    Issue-#17 - Implementação da classe de teste de atribuição do capítulo 02

--- app/src/test/java/oca/chapter02/AttributionTest.java
+++ app/src/test/java/oca/chapter02/AttributionTest.java
@@ -1,4 +1,42 @@
 package oca.chapter02;
 
-public class Atribution {
+import oca.chapter02.primitives.example01.Attribution;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * <br>Testes para verificar a funcionalidade da classe Attribution.
+ * <br>Esses testes são importantes para garantir que a classe Attribution esteja funcionando conforme o esperado, realizando atribuições corretamente e lidando com exceções de forma adequada.
+ *
+ * @author henriqueotogami
+ * @since 2026-02-23
+ * @version 0.4
+ * @see Attribution
+ * @throws Exception
+ */
+@TestMethodOrder(org.junit.jupiter.api.MethodOrderer.OrderAnnotation.class)
+public class AttributionTest {
+
+    /**
+     * <br>Teste para verificar se a classe Attribution realiza atribuições corretamente e lida com exceções de forma adequada.
+     * <br>Este teste é crucial para garantir que a classe Attribution esteja funcionando conforme o esperado, realizando atribuições corretamente e lançando exceções quando necessário.
+     *
+     * @author henriqueotogami
+     * @since 2026-02-23
+     * @version 0.4
+     * @see Attribution
+     * @throws Exception
+     */
+    @Test
+    @Order(1)
+    public void fishTest() {
+        Attribution attribution = new Attribution();
+        attribution.setDefaultDegrees();
+        attribution.sumFishes(100, 50);
+        Assertions.assertThrows(NumberFormatException.class, () -> { attribution.setFoundTotalFish("FISH"); });
+        Assertions.assertEquals(100, attribution.getDegrees());
+        Assertions.assertEquals(150, attribution.getTotalFish());
+    }
 }
