aa481976d9af22c9151d4192d0514dc5212dd2e4
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-02-25 07:14:25 -0300

    Issue #17 - Implementação de classe de condicionais

--- app/src/main/java/oca/chapter02/primitives/example02/Conditionals.java
+++ app/src/main/java/oca/chapter02/primitives/example02/Conditionals.java
@@ -1,4 +1,68 @@
 package oca.chapter02.primitives.example02;
 
-public class IfThenElse {
+/**
+ * <br>Classe representando as condições em Java.
+ * <br>Demonstrando o uso de condições em Java, como if, else, switch, ternário, etc.
+ *
+ * @author henriqueotogami
+ * @since 2026-02-25
+ * @version 1.0
+ */
+public class Conditionals {
+
+    public boolean isItRaining = false;
+
+    /**
+     * <br>Obtém o tempo atual.
+     * <br>Este metodo é importante para demonstrar como obter o tempo atual, demonstrando o conceito de encapsulamento em Java.
+     *
+     * @return O tempo atual.
+     */
+    public String getWeatherByDefault() {
+        if (isItRaining) {
+            return "It's raining!";
+        } else {
+            return "It's not raining!";
+        }
+    }
+
+    /**
+     * <br>Obtém o tempo atual.
+     * <br>Este metodo é importante para demonstrar como obter o tempo atual, demonstrando o conceito de encapsulamento em Java.
+     *
+     * @return O tempo atual.
+     */
+    public String getWeatherByTernary() {
+        return isItRaining ? "It's raining!" : "It's not raining!";
+    }
+
+    /**
+     * <br>Obtém o tempo atual.
+     * <br>Este metodo é importante para demonstrar como obter o tempo atual, demonstrando o conceito de encapsulamento em Java.
+     *
+     * @return O tempo atual.
+     */
+    public String getWeatherWithSwitch() {
+        switch (String.valueOf(isItRaining).toLowerCase()) {
+            case "true":
+                return "It's raining!";
+            case "false":
+                return "It's not raining!";
+            default:
+                return "Unknown weather condition!";
+        }
+    }
+
+    /**
+     * <br>Método principal.
+     * <br>Este metodo é importante para demonstrar como executar o programa, demonstrando o conceito de encapsulamento em Java.
+     *
+     * @param args Argumentos da linha de comando.
+     */
+    public static void main(String[] args) {
+        final Conditionals conditionals = new Conditionals();
+        System.out.println(conditionals.getWeatherByDefault());
+        System.out.println(conditionals.getWeatherByTernary());
+        System.out.println(conditionals.getWeatherWithSwitch());
+    }
 }
