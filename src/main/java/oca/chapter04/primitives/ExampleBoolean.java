8b5a0386371a47c99a92f2dc4de766718cd2ccfa
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-04-04 13:58:02 -0300

    Issue-#33 - Exemplo de tipo Primitivo - boolean

--- src/main/java/oca/chapter04/primitives/ExampleBoolean.java
+++ src/main/java/oca/chapter04/primitives/ExampleBoolean.java
@@ -1,4 +1,20 @@
 package oca.chapter04.primitives;
 
+/**
+ * <br>Exemplo de uso do tipo primitivo boolean em Java.
+ *
+ * @author henriqueotogami
+ * @since 2026-04-04
+ * @version 0.5
+ */
 public class ExampleBoolean {
+
+    private boolean hasTurboCharger = false;
+
+    public void activateTurboCharger() {
+        this.hasTurboCharger = true;
+    }
+    public boolean getTurboCharger() {
+        return this.hasTurboCharger;
+    }
 }
