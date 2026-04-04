db09a1fa29eb28879ca92752e5b7fba6d1608ac3
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-04-04 13:58:53 -0300

    Issue-#33 - Teste Unitário - Tipo primitivo boolean

--- src/test/java/oca/chapter04/ExamplesTest.java
+++ src/test/java/oca/chapter04/ExamplesTest.java
@@ -1,4 +1,29 @@
 package oca.chapter04;
 
-public class ExamplesTest {
+import oca.chapter04.primitives.ExampleBoolean;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <br>Classe de teste para os exemplos de tipos primitivos e objetos em Java.
+ *
+ * @author henriqueotogami
+ * @since 2026-04-04
+ * @version 0.5
+ */
+class ExamplesTest {
+
+    /**
+     * Teste para o exemplo de uso do tipo primitivo boolean.
+     */
+    @Test
+    @Order(1)
+    void example01Boolean() {
+        final ExampleBoolean exampleBoolean = new ExampleBoolean();
+        Assertions.assertFalse(exampleBoolean.getTurboCharger());
+
+        exampleBoolean.activateTurboCharger();
+        Assertions.assertTrue(exampleBoolean.getTurboCharger());
+    }
 }
