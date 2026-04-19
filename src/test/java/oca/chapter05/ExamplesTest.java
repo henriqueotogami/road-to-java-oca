26b3d54c7912b3a32e8df6bb61bd4a56a8d58ae2
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-04-19 20:01:38 -0300

    Issue-#36 - Teste Unitário - Exemplo 01

--- src/test/java/oca/chapter05/ExamplesTest.java
+++ src/test/java/oca/chapter05/ExamplesTest.java
@@ -1,4 +1,46 @@
 package oca.chapter05;
 
-public class ExamplesTest {
+import oca.chapter05.methods.example01.MathTools;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <br>Classe de teste para os exemplos de métodos em Java.
+ *
+ * @author henriqueotogami
+ * @since 2026-04-19
+ * @version 0.5
+ */
+class ExamplesTest {
+
+    /**
+     * Teste para o exemplo de uso de métodos em Java.
+     */
+    @Test
+    @Order(1)
+    void example01CreateMethod() {
+        final MathTools mTools = new MathTools();
+        final int x = 8;
+        final int y = 13;
+        final int lowestInt = mTools.findLowerValue(x,y);
+        Assertions.assertEquals(8, lowestInt);
+        Assertions.assertEquals(8, mTools.findLowerValue(x,y));
+    }
+
+    /**
+     * Teste para o exemplo de uso de métodos sobrecarregados em Java.
+     */
+    @Test
+    @Order(2)
+    void example01OverloadMethod() {
+        final MathTools mTools = new MathTools();
+        final double x = 8.5;
+        final double y = 13.2;
+        final double lowestDouble = mTools.findLowerValue(x,y);
+        Assertions.assertEquals(8.5, lowestDouble);
+        Assertions.assertEquals(8.5, mTools.findLowerValue(x,y));
+    }
+
+
 }
