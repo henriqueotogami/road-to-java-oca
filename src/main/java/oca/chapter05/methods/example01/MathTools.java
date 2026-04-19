da5a8651210fb67ffc8cc62dff250c53232869ba
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-04-19 20:11:00 -0300

    Issue-#36 - Exemplo 02 - Método sobrecarregado

--- src/main/java/oca/chapter05/methods/example01/MathTools.java
+++ src/main/java/oca/chapter05/methods/example01/MathTools.java
@@ -9,6 +9,13 @@ package oca.chapter05.methods.example01;
  */
 public class MathTools {
 
+    /**
+     * Metodo para encontrar o menor valor entre dois números inteiros.
+     *
+     * @param number1 O primeiro número inteiro a ser comparado.
+     * @param number2 O segundo número inteiro a ser comparado.
+     * @return O menor valor entre os dois números inteiros fornecidos.
+     */
     public int findLowerValue(final int number1, final int number2) {
         int result;
         if (number1 < number2)
@@ -18,6 +25,13 @@ public class MathTools {
         return result;
     }
 
+    /**
+     * Metodo para encontrar o menor valor entre dois números de ponto flutuante.
+     *
+     * @param number1 O primeiro número de ponto flutuante a ser comparado.
+     * @param number2 O segundo número de ponto flutuante a ser comparado.
+     * @return O menor valor entre os dois números de ponto flutuante fornecidos.
+     */
     public double findLowerValue(final double number1, final double number2){
         double result;
         if(number1 < number2)
