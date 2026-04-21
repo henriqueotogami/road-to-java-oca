bf84377299c4bd989f3cc6379c9d246d95c46698
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-04-20 21:19:16 -0300

    Issue-#36 - Exemplo 06 - Criação de Construtores

---
+++ src/main/java/oca/chapter05/methods/example06/LoanDetails.java
@@ -0,0 +1,29 @@
+package oca.chapter05.methods.example06;
+
+/**
+ * <br>Classe de exemplo para demonstrar o uso de métodos em Java.
+ *
+ * @author henriqueotogami
+ * @since 2026-04-20
+ * @version 0.5
+ */
+public class LoanDetails {
+
+    private int term;
+    private double rate;
+    private double principal;
+
+    public LoanDetails() {
+        term = 180;
+        rate = .0265; //Taxa de juros em formato decimal
+        principal = 0;
+    }
+
+    public void setPrincipal(double p) {
+        principal = p;
+    }
+
+    public double monthlyPayment() {
+        return (rate * principal / 12) / (1.0 - Math.pow(((rate / 12) + 1.0), (-term)));
+    }
+}
