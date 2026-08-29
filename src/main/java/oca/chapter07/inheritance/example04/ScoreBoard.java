42a17ad9ba5b8db65a71dfaa809fca7057cb2d89
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2026-08-29 13:48:51 -0300

    Issue-#40 - Implementação de exemplo de uso de getters e setters

---
+++ src/main/java/oca/chapter07/inheritance/example04/ScoreBoard.java
@@ -0,0 +1,17 @@
+package oca.chapter07.inheritance.example04;
+
+/**
+ * Classe que representa um placar de pontuação.
+ * Esta classe fornece métodos para obter e definir a pontuação atual.
+ *
+ * @author henriqueotogami
+ * @since 2026-08-29
+ * @version 0.9
+ */
+public class ScoreBoard {
+
+    private int score;
+
+    public int getScore() { return this.score; }
+    public void setScore(int score) { this.score = score; }
+}
---
+++ src/main/java/oca/chapter07/inheritance/example05/PhoneBookEntry.java
@@ -0,0 +1,36 @@
+package oca.chapter07.inheritance.example04;
+
+/**
+ * Classe que representa uma entrada na agenda telefônica.
+ * Esta classe fornece métodos para obter e definir o nome, número de telefone e a última atualização da entrada.
+ *
+ * @author henriqueotogami
+ * @since 2026-08-29
+ * @version 0.9
+ */
+public class PhoneBookEntry {
+
+    private String name = "";
+    private int phoneNumber = 0;
+    private long lastUpdate = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNameNumber(String name,int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        lastUpdate = System.currentTimeMillis();
+    }
+
+}
